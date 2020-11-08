7051883e4cfe7527f5d2096803a4924af579851c
Zele Zoltan <zelezoltan@inf.elte.hu>
2020-11-09 00:54:36 +0100

    Rename

--- GameOfLife.View.Presentation/MainWindow.xaml.cs
+++ GameOfLife.View.Presentation/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GameOfLife
+namespace GameOfLife.View.Presentation
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
